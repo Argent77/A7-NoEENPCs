4c2df1470921994fa14d1be05a7805d31d61cc00
Argent77 <4519923+Argent77@users.noreply.github.com>
2021-05-30 14:07:30 +0200

    Improve BG2 passive Neera; fix viewport location in Neera's random encounter area

--- A7-ConvenientEENPCs/dialogs/neera_bg2.d
+++ A7-ConvenientEENPCs/dialogs/neera_bg2.d
@@ -7,15 +7,10 @@ IF WEIGHT #-1 ~NumberOfTimesTalkedTo(0) AreaCheck("AR0500") Global("A7_NOEE_NEER
   = @10001  /* Huh? What's this? */
   IF ~~ DO ~
   SetGlobal("A7_NOEE_NEERA","AR0500",2)
-  SetNumTimesTalkedTo(0)
   SetGlobal("OHN_INTRO_SCENE","AR0500",2)
-  CreateCreature("OHNMERET",[3810.378],SW)
-  CreateCreature("OHNLANNE",[3921.340],SW)
-  CreateCreature("OHNREDW",[3769.437],NE)
-  CreateCreature("OHNWARWI",[3721.410],E)
-  CreateCreature("OHNELLIS",[3848.448],NNW)
+  SetNumTimesTalkedTo(0)
   ClearAllActions()
-  StartCutScene("OHNCUT01")~ EXIT
+  StartCutScene("A7_NCUT1")~ EXIT
 END
 
 END
